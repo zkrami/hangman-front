ab2e7f61ff1e7ad1eaa1965ca533a650f7a7d2a3
Rexar <rexar.fr@gmail.com>
2020-06-14 13:42:01 +0200

    socket io

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'; 
+class App extends React.Component<any , any> {
+  socket: SocketIOClient.Socket | null = null;
+  
+ 
+  constructor(props:any){
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      connected : false , 
+      messages : []  , 
+      serverMessage : ""   
+    }; 
+   
+  }
+  componentDidMount(){
+     
+    const socket = io("http://localhost:3000" , {path : "/server-path"});     
+    socket.on("connect" , () => {
+       console.log("connected to server"); 
+       this.setState({connected : true}); 
+    });
+    socket.on("disconnect" , ()=> {
+      console.log("disconnected to server"); 
+      this.setState({connected : false}); 
+
+    }); 
+    socket.on("server-message" , (data:any) => {
+        this.setState( {messages : [...this.state.messages , data.message ]  });
+    }); 
+    
+    this.socket = socket;  
+
+  }
+  
+  send(){    
+    if(this.socket)      
+      this.socket.emit("message" , { message : this.state.message}); 
+  }
+  changeValue(event:any){
+    this.setState({message : event.target.value }); 
+  }
+  
+  render(){
+    
+    return (
+      <div className="App">
+        <div>
+        <p>Working</p>
+         <p>{this.state.connected && <span>Connected </span>}</p>
+         <p>{!this.state.connected && <span>Disconneted </span>}</p>
+
+        </div>
+        <div >
+          <input placeholder="Message" onChange={this.changeValue.bind(this)}  />
+          <button onClick={() => this.send()} >Send Data</button>
+        </div>
+        <div>
+          <p>Serve Message</p>
+          {this.state.messages.map( (m:any) => <p>{m}</p>)}
+
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
